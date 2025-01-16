5c40c87cb818cc1013e0cc0fb76e42fd4cea6efb
Rayleen Roberts <rayleen.roberts@student.centralia.edu>
2025-01-16 12:39:22 -0800

    Add project files.

---
+++ DataMining2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ML;
+
+namespace DataMining2
+{
+    public class Program
+    {
+        private MLContext ctx;
+        private string dataPath;
+        private IDataView trainingData;
+        private string modelPath;
+        private ITransformer trainedModel;
+
+        // prediction engine with input and output types
+        // put variables here to make them global without making them global through the namespace
+        public Program()
+        {
+            // Gather any variables and set them 
+            dataPath = Path.Combine(Environment.CurrentDirectory, "data\\DisneylandReviews.csv");
+            modelPath = Path.Combine(Environment.CurrentDirectory, "models\\model.zip");
+
+            // Create a (machine learning context) Context () will be a member of the program class variables
+            ctx = new MLContext();
+            trainingData = ctx.Data.LoadFromTextFile<DisneylandReview>(dataPath, hasHeader: true);
+            // read the input data into the system
+            // build a data pipeline (transforming data into something that works)
+            // train model (make it run the guantlet)
+            // consume the model (make predictions)
+        }
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
